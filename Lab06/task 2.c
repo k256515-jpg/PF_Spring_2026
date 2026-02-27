4a34523cc5c5348d84d3677721694bff26113a3a
k256515-jpg <k256515@nu.edu.pk>
2026-02-27 10:42:51 +0500

    Update task 2.c

--- Lab06/task 2.c
+++ Lab06/task 2.c
@@ -1,1 +1,13 @@
-
+#include<stdio.h>
+int main(){
+	int n,withdrawl;
+	printf("current bank balance: ");
+	scanf("%d",&n);
+	while(n>0){
+		printf("how much do you want to withdrawl?: ",withdrawl);
+		scanf("%d",&withdrawl);
+		n=n-withdrawl;
+	}
+	printf("\nyour balance is now 0.");
+	return 0;
+}
