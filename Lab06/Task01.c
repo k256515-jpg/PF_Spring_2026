2c666b19e2578e29f0c343ac02ff888be3cebcba
k256515-jpg <k256515@nu.edu.pk>
2026-02-27 10:26:11 +0500

    Update Task01.c

--- Lab06/Task01.c
+++ Lab06/Task01.c
@@ -1,1 +1,14 @@
-
+#include<stdio.h>
+int main(){
+	int attendance,total=0;
+		for(int i=1;i<=30;i++){
+	printf("enter attendance of student(use 1 for present, 0 for absent): ",i);
+	scanf("%d",&attendance);
+	
+		if(attendance==1){
+			total++;
+		}
+	}
+	printf("\nthe total number of students present is %d ",total);
+	return 0;
+}
