e24e77e62031bbb580b786e57bd5ce77be70aceb
k256515-jpg <k256515@nu.edu.pk>
2026-02-27 11:03:47 +0500

    Update task 4.c

--- Lab06/task 4.c
+++ Lab06/task 4.c
@@ -1,1 +1,12 @@
-
+#include<stdio.h>
+int main(){
+	int n,sum=0;
+	printf("enter n: ");
+	scanf("%d",&n);
+	for(int i=1;i<=n;i++){
+		sum+=i;
+		
+	}
+	printf("the sum of n natural number is %d",sum);
+	return 0;
+}
